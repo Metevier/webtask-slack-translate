85e1fdf1448520c1ce9a981dff67024f258c63ed
Tyler Metevier <tyler.metevier@gmail.com>
2017-06-05 12:04:10 -0400

    Add languages endpoint

--- src/endpoints.js
+++ src/endpoints.js
@@ -1,14 +1,21 @@
 const baseEndpoint = 'https://translation.googleapis.com/language/translate/v2';
 
 export default function(apiKey) {
+  const languagesEndpoint   = `${baseEndpoint}/languages?key=${apiKey}`;
   const translationEndpoint = `${baseEndpoint}?key=${apiKey}`;
+
+  const getLanguages = () => {
+    return;
+  }
 
   const getTranslation = () => {
     return;
   }
 
   return {
+    getLanguages,
     getTranslation,
+    languagesEndpoint,
     translationEndpoint
   }
 }
--- test/endpoints.spec.js
+++ test/endpoints.spec.js
@@ -4,14 +4,26 @@ import endpoints from '../src/endpoints';
 describe('endpoints', () => {
   const key = 'supersecretkey';
 
-  it ('Should build a translationEndpoint with API_KEY', () => {
-    const expectedTranslationEnpoint = `https://translation.googleapis.com/language/translate/v2?key=${key}`;
-    const { translationEndpoint } = endpoints(key);
-    expect(translationEndpoint).to.equal(expectedTranslationEnpoint);
+  it ('Should build a languagesEndpoint with API_KEY', () => {
+    const expectedLanguagesEndpoint = `https://translation.googleapis.com/language/translate/v2/languages?key=${key}`;
+    const { languagesEndpoint } = endpoints(key);
+    expect(languagesEndpoint).to.equal(expectedLanguagesEndpoint);
   }); 
+
+  it ('Should build a translationEndpoint with API_KEY', () => {
+    const expectedTranslationEndpoint = `https://translation.googleapis.com/language/translate/v2?key=${key}`;
+    const { translationEndpoint } = endpoints(key);
+    expect(translationEndpoint).to.equal(expectedTranslationEndpoint);
+  }); 
+  
+  it('Should return getLanguages()', () => {
+    const { getLanguages } = endpoints(key);
+    expect(getLanguages).to.be.a('function');
+  });
 
   it('Should return getTranslation()', () => {
     const { getTranslation } = endpoints(key);
     expect(getTranslation).to.be.a('function');
   });
+
 });
